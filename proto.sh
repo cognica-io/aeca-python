139e21307dabf04bc74ad63f0a7430478e7f521f
Jaepil Jeong <jaepil@aeca.ai>
2024-06-12 16:36:12 +0900

    Update build script

--- proto.sh
+++ proto.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 SRC=src/aeca/protobuf
-FILES=(document_db.proto key_value_db.proto fts_analysis_pipeline.proto sentence_transformer.proto)
+FILES=(document_db.proto key_value_db.proto fts_analysis_pipeline.proto sentence_transformer.proto system_metrics.proto)
 
 python -m grpc_tools.protoc -I $SRC --python_out=$SRC $SRC/document.proto
 
